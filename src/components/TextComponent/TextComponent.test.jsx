a096b4dbc48a66060a87e55689a0c72b42462814
Henrique-Aguiar <hrikelr@hotmail.com>
2022-12-14 20:52:09 -0300

    test textComponent

--- src/components/TextComponent/TextComponent.test.jsx
+++ src/components/TextComponent/TextComponent.test.jsx
@@ -3,9 +3,22 @@ import { renderTheme } from '../../styles/render-theme';
 import { TextComponent } from '.';
 
 describe('<TextComponent />', () => {
-  it('should render', () => {
+  it('should render a text', () => {
     renderTheme(<TextComponent>texto</TextComponent>);
+    expect(screen.getByText('texto')).toBeInTheDocument();
+  });
+  it('should match snapshot', () => {
+    const { container } = renderTheme(<TextComponent>texto</TextComponent>);
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
+        font-size: 2.4rem;
+      }
 
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+      <p
+        class="c0"
+      >
+        texto
+      </p>
+    `);
   });
 });
