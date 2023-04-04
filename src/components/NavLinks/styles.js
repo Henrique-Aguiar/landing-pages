e99a4563597541ec2dbbf01f5e2903be392f337c
Henrique-Aguiar <hrikelr@hotmail.com>
2023-04-03 21:44:24 -0300

    corrigindo styled

--- src/components/NavLinks/styles.js
+++ src/components/NavLinks/styles.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import styled, { css } from 'styled-components';
-export const Container = styled.div`
+export const Container = styled.nav`
   ${({ theme }) => css`
     display: flex;
     flex-flow: row wrap;
