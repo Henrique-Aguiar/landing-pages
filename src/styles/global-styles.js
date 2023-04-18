9e0406aafbd661f7eb5c880fc25c85743d14555e
Henrique-Aguiar <hrikelr@hotmail.com>
2023-04-18 19:33:18 -0300

    GridText

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -13,12 +13,12 @@ export const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    font-family: ${theme.font.family.default};
+    font-family: ${({ theme }) => theme.font.family.default};
     font-size: 1.6rem;
   }
 
   h1, h2, h3, h4, h5, h6 {
-    font-family: ${theme.font.family.secondary};
+    font-family: ${({ theme }) => theme.font.family.secondary};
     margin: ${({ theme }) => theme.spacings.large} 0;
   }
 
