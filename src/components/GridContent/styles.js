7ae4540882178407564e1a3bfdaef57d2eecf698
Henrique-Aguiar <hrikelr@hotmail.com>
2023-04-18 17:29:13 -0300

    GridContent

---
+++ src/components/GridContent/styles.js
@@ -0,0 +1,12 @@
+import styled, { css } from 'styled-components';
+export const Container = styled.div`
+  ${({ theme }) => css`
+
+  `}
+`;
+
+export const Html = styled.div`
+  ${({ theme }) => css`
+    /* margin: ${theme.spacings.large} 0; */
+  `}
+`;
