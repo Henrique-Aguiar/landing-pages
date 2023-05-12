b49d608b8ed20a168ff5c42bacaabcd041ff8d3f
Henrique-Aguiar <hrikelr@hotmail.com>
2023-05-11 21:36:21 -0300

    TDD

--- src/components/Footer/stories.jsx
+++ src/components/Footer/stories.jsx
@@ -4,7 +4,7 @@ export default {
   title: 'Footer',
   component: Footer,
   args: {
-    html: `<p><a href="https://github.com/Henrique-Aguiar">Feito com ❤ por Henrique Dias</a></p>`,
+    footerHtml: `<p><a href="https://github.com/Henrique-Aguiar">Feito com ❤ por Henrique Dias</a></p>`,
   },
 };
 
--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -1,7 +1,7 @@
 import * as Styled from './styles';
 import { Heading } from './../../components/Heading';
 import { Base } from '../Base';
-import { mockBase } from '../Base/stories';
+import { mockBase } from '../Base/mock';
 
 function Home() {
   return <Base {...mockBase} />;
